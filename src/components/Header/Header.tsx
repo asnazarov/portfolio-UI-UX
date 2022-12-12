20ed8efede5014d6d29790f436624a6ed6a32d71
Alexander <asn.nazarov.asn@gmail.com>
2022-12-12 20:31:31 +0300

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,30 @@
 import style from './style.module.scss'
+import Link from "next/link";
 
 const Header = () => {
 
   return (
     <header className={style.header}>
-      header
+      <nav className={style.nav}>
+        <ul className={style.list}>
+          <div/>
+          <li className={style.link}>
+            <Link href={'/'}>
+              Главная
+            </Link>
+          </li>
+          <li className={style.link}>
+            <Link href={'/'}>
+              Проекты
+            </Link>
+          </li>
+          <li className={style.link}>
+            <Link href={'/'}>
+              Контакты
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
