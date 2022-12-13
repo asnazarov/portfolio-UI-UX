16364de27813b75a3eb39204ca4e3d368beea139
Alexander <asn.nazarov.asn@gmail.com>
2022-12-13 18:01:13 +0300

    deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,31 +15,30 @@ export default function Home() {
       <Head>
         {/*<meta name="viewport" content="width=device-width, initial-scale=1"/>*/}
         {/*<title>Портфолио А.С. Назарова</title>*/}
-        <meta property="og:title" content="My page title" key="title" />
-        {/*<meta property="og:image"*/}
-        {/*      content={'../src/assets/img.png'}/>*/}
+        <meta property="og:title" content="Портфолио А.С. Назарова" key="title" />
+        <meta property="og:image" content={'../src/assets/img.png'}/>
         <meta property='og:description' content="Сюда пожно добавить описание страницы." />
         {/*<meta name="description" content="Сюда пожно добавить описание страницы." />*/}
         <link rel="shortcut icon" href={favicon.src} type="image/x-icon"/>
         {/* <!-- Yandex.Metrika counter -->*/}
-        <Script
-          id="show-banner"
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        {/*<Script*/}
+        {/*  id="show-banner"*/}
+        {/*  type="text/javascript"*/}
+        {/*  dangerouslySetInnerHTML={{*/}
+        {/*    __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};*/}
+        {/*  m[i].l=1*new Date();*/}
+        {/*  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}*/}
+        {/*  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})*/}
+        {/*  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");*/}
 
-          ym(91654445, "init", {
-          clickmap:true,
-          trackLinks:true,
-          accurateTrackBounce:true,
-          webvisor:true
-        })`,
-          }}
-        />
+        {/*  ym(91654445, "init", {*/}
+        {/*  clickmap:true,*/}
+        {/*  trackLinks:true,*/}
+        {/*  accurateTrackBounce:true,*/}
+        {/*  webvisor:true*/}
+        {/*})`,*/}
+        {/*  }}*/}
+        {/*/>*/}
         {/*<noscript>*/}
         {/*  dangerouslySetInnerHTML={{*/}
         {/*    __html: `<div><img src="https://mc.yandex.ru/watch/91654445" style="position:absolute; left:-9999px;" alt="" /></div>`*/}
