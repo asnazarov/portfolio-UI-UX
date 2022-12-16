1ca78c26b4cb6861c9edb5b291e35049cf2ed4a3
Alexander <asn.nazarov.asn@gmail.com>
2022-12-16 22:52:11 +0300

    deploy

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,37 +2,67 @@ import style from './style.module.scss'
 import Link from "next/link";
 import Burger_svg from "../../assets/svg/Burger_svg";
 import {useWindowSize} from "../../utils/js-utils";
+import {useState} from "react";
+import NavModal from "../UI/Modals/Nav-modal/Nav-modal";
+
+export type NavType = {
+  id: number,
+  text: string,
+  link: string
+}[]
+
+const nav: NavType = [
+  {
+    id: 1,
+    text: 'главная',
+    link: '/'
+  },
+  {
+    id: 2,
+    text: 'Проекты',
+    link: '/'
+  },
+  {
+    id: 3,
+    text: 'Контакты',
+    link: '/'
+  },
+]
+
 
 const Header = () => {
   const size = useWindowSize()
+  const [open, setOpen] = useState<boolean>(false)
 
+  console.log(open)
 
   return (
     <header className={style.header}>
       <nav className={style.nav}>
-        {size.width < 500 && (
-          <div className={style.nav__burger}>
-            <Burger_svg/>
-          </div>
+        {size.width < 500 ? (
+          <>
+            <div className={style.nav__burger} onClick={() => setOpen(!open)}>
+              <Burger_svg/>
+            </div>
+            {
+              open && <NavModal nav={nav}/>
+            }
+          </>
+        ) : (
+          <ul className={style.list}>
+            {nav?.map(el => (
+              <li className={style.link} key={el.id}>
+                <Link href={el.link}>
+                  {el.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
         )}
-        <ul className={style.list}>
-          <li className={style.link}>
-            <Link href={'/'}>
-              Главная
-            </Link>
-          </li>
-          <li className={style.link}>
-            <Link href={'/'}>
-              Проекты
-            </Link>
-          </li>
-          <li className={style.link}>
-            <Link href={'/'}>
-              Контакты
-            </Link>
-          </li>
-        </ul>
+
       </nav>
+
+
     </header>
   )
 }
--- src/components/UI/Modals/Nav-modal/Nav-modal.tsx
+++ src/components/UI/Modals/Nav-modal/Nav-modal.tsx
@@ -1,13 +1,25 @@
 import styles from './styles.module.scss'
+import {NavType} from "../../../Header/Header";
+import {FC} from "react";
+import Link from "next/link";
 
-const NavModal = () => {
+
+type OwnPropertyType = {
+  nav: NavType
+}
+
+const NavModal: FC<OwnPropertyType> = (props) => {
 
   return (
-    <nav>
-      <ul>
-        123
-      </ul>
-    </nav>
+    <ul className={styles.list}>
+      {
+        props.nav?.map(el => (
+          <li className={styles.link} key={el.id}>
+            <Link href={el.link}>{el.text}</Link>
+          </li>
+        ))
+      }
+    </ul>
   )
 }
 
