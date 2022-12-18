6015564fa1e370cd5bde96ffda49ff6f072c3e64
Alexander <asn.nazarov.asn@gmail.com>
2022-12-18 23:03:02 +0300

    deploy

--- src/features/About/About.tsx
+++ src/features/About/About.tsx
@@ -14,32 +14,32 @@ const About = () => {
       </div>
       <div className={styles.about__content}>
         <m.p
-          initial={{x: '100%', opacity: 0}}
+          initial={{x: '10%', opacity: 0}}
           animate={{x: '0%', opacity: 1}}
           transition={{delay: .5, duration: .5, ease: "easeOut"}}
           className={styles.hello}>Привет</m.p>
         <m.h2
-          initial={{x: '100%', opacity: 0}}
+          initial={{x: '10%', opacity: 0}}
           animate={{x: '0%', opacity: 1}}
           transition={{delay: .7, duration: .5, ease: "easeOut"}}
           className={styles.title}>Меня зовут Анастасия назарова.</m.h2>
         <m.h1
-          initial={{x: '100%', opacity: 0}}
+          initial={{x: '10%', opacity: 0}}
           animate={{x: '0%', opacity: 1}}
           transition={{delay: .9, duration: .5, ease: "easeOut"}}
           className={`${styles.title} ${styles.title_style}`}>Я Ui/Ux дизайнер из санкт-петербурга</m.h1>
         <m.p
-          initial={{x: '100%', opacity: 0}}
+          initial={{x: '10%', opacity: 0}}
           animate={{x: '0%', opacity: 1}}
           transition={{delay: 1.1, duration: .5, ease: "easeOut"}}
           className={styles.specialization}><Arrow_1_svg /> Специализация</m.p>
         <m.p
-          initial={{x: '100%', opacity: 0}}
+          initial={{x: '10%', opacity: 0}}
           animate={{x: '0%', opacity: 1}}
           transition={{delay: 1.1, duration: .5, ease: "easeOut"}}
           className={styles.specialization}>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</m.p>
         <m.p
-          initial={{x: '100%', opacity: 0}}
+          initial={{x: '10%', opacity: 0}}
           animate={{x: '0%', opacity: 1}}
           transition={{delay: 1.3, duration: .5, ease: "easeOut"}}
           className={styles.open}>Открыта к предложениями <br/> по сотрудничеству</m.p>
