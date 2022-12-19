05bfab11dbf2d65e1ac313e58cb70b0ddf4dc278
Alexander <asn.nazarov.asn@gmail.com>
2022-12-19 21:35:10 +0300

    deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         <meta name="twitter:card" content="summary" key="twcard"/>
         <meta name="twitter:creator" content={photo.src} key="twhandle"/>
 
-
+        <title>Портфолио А.С. Назарова</title>
         <meta property="og:title" content="Портфолио А.С. Назарова" key="title"/>
         <meta property='og:description' content="Сюда пожно добавить описание страницы."/>
         <meta property="og:image" content={photo.src}/>
@@ -40,19 +40,18 @@ export default function Home() {
         {/* <!-- Yandex.Metrika counter -->*/}
         {/*// <!-- Yandex.Metrika counter -->*/}
         <script type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-          ym(91767756, "init", {
-          defer=true,
-          clickmap:true,
-          trackLinks:true,
-          accurateTrackBounce:true
-        })`
-        }}
+                dangerouslySetInnerHTML={{
+                  __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        ym(91767756, "init", {
+        defer=true,
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+          })`,}}
         >
         </script>
         {/*<noscript><div><img src="https://mc.yandex.ru/watch/91767756" style="position:absolute; left:-9999px;" alt="" /></div></noscript>*/}
@@ -62,19 +61,16 @@ export default function Home() {
           type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-          ym(91654445, "init", {
-          defer=true,
-          clickmap:true,
-          trackLinks:true,
-          accurateTrackBounce:true,
-          webvisor:true
-        })`,
-          }}
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        ym(91767756, "init", {
+        defer=true,
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+          })`,}}
         />
         {/*<noscript>*/}
         {/*  dangerouslySetInnerHTML={{*/}
