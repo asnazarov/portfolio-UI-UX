7c6fe1a0373d4e20bb3d65532acabeec6f812487
Alexander <asn.nazarov.asn@gmail.com>
2022-12-19 21:31:28 +0300

    deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,26 @@ export default function Home() {
         <meta property="og:type" content="website"/>
         <link rel="shortcut icon" href={favicon.src} type="image/x-icon"/>
         {/* <!-- Yandex.Metrika counter -->*/}
-        <Script
+        {/*// <!-- Yandex.Metrika counter -->*/}
+        <script type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+          ym(91767756, "init", {
+          defer=true,
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true
+        })`
+        }}
+        >
+        </script>
+        {/*<noscript><div><img src="https://mc.yandex.ru/watch/91767756" style="position:absolute; left:-9999px;" alt="" /></div></noscript>*/}
+        {/*// <!-- /Yandex.Metrika counter -->*/}
+        <script
           id="show-banner"
           type="text/javascript"
           dangerouslySetInnerHTML={{
@@ -49,6 +68,7 @@ export default function Home() {
           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
           ym(91654445, "init", {
+          defer=true,
           clickmap:true,
           trackLinks:true,
           accurateTrackBounce:true,
